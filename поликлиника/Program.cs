933d390874e455cb759cd6071f3b5bc8e0b71837
79612 <79612@DESKTOP-O1P5TC5>
2024-02-20 13:10:03 +0300

    Добавьте файлы проекта.

---
+++ поликлиника/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace поликлиника
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int receptionTime = 10;
+            int visitor = 14;
+            int waitingTime = receptionTime * visitor;
+            int hour = waitingTime / 60;
+            int minute = waitingTime % 60;
+
+            Console.WriteLine($"Вы должны стоять в очереди{hour} часа и{minute} минут.");
+
+        }
+    }
+}
